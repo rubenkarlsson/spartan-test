466809753cf6b44f6af67c9e6a86414140000347
rubenkarlsson <ruben.karlsson@teclead-ventures.de>
2024-10-15 15:58:15 +0200

    initial commit

---
+++ spartan-test-1/src/app/pages/(home).page.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+import { AnalogWelcomeComponent } from "./analog-welcome.component";
+
+@Component({
+  selector: "spartan-test-1-home",
+  standalone: true,
+  imports: [AnalogWelcomeComponent],
+  template: ` <spartan-test-1-analog-welcome /> `,
+})
+export default class HomeComponent {}
